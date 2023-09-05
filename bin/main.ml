ecc810f6074eb7516f43c0b93409ed0060a385f8
Eduardo Stocchero <eduardostocchero@proton.me>
2023-09-05 14:08:49 -0300

    feat: parse infix and add builtin functions (#8)

--- bin/main.ml
+++ bin/main.ml
@@ -10,17 +10,47 @@ let builtin_exit = OBuiltIn (function OInt i -> exit i; | _ -> OUnit)
 
 let exit_typ = Scheme ([], TFn (t_int, Infer.new_hole ()))
 
+let arith op =
+  OBuiltIn (function OInt x ->
+    OBuiltIn (function OInt y -> OInt (op x y) | _ -> OUnit) | _ -> OUnit)
+
+let arith_typ = Scheme ([], TFn (t_int, TFn (t_int, t_int)))
+
+let builtin_mul = arith ( * )
+let builtin_plus = arith ( + )
+let builtin_min = arith ( - )
+let builtin_div = arith ( / )
+
+let builtin_inc = OBuiltIn (function OInt i -> OInt (i + 1) | _ -> OUnit)
+
+let inc_typ = Scheme ([], TFn (t_int, t_int))
+
 let type_ctx =
-  [("exit", exit_typ)]
+  [
+    ("exit", exit_typ);
+    ("$plus", arith_typ);
+    ("$min", arith_typ);
+    ("$div", arith_typ);
+    ("$mul", arith_typ);
+    ("inc", inc_typ);
+  ]
   |> List.to_seq
   |> Infer.Ctx.of_seq
 
 let std_env =
-  [("exit", builtin_exit)]
+  [
+    ("exit", builtin_exit);
+    ("$plus", builtin_plus);
+    ("$min", builtin_min);
+    ("$div", builtin_div);
+    ("$mul", builtin_mul);
+    ("inc", builtin_inc);
+  ]
   |> List.to_seq
   |> Env.of_seq
 
 let rec repl () =
+  let () = print_string "> " in
   let s = read_line () in
   let opt_p = Parser.program Lexer.read_token (Lexing.from_string s) in
   let s = match opt_p with
