3059cbf4e608d2ee10cbdcb1fe4f36c49e1c1998
edustocchero <eduardostocchero@proton.me>
2023-09-05 18:26:33 -0300

    chore: add code formatter

--- bin/main.ml
+++ bin/main.ml
@@ -2,26 +2,38 @@ open Asdf
 open Eval.Env
 open Eval.Evaluator
 open Eval.Object
-
 open Typer
 open Typer.Types
 
-let builtin_exit = OBuiltIn (function OInt i -> exit i; | _ -> OUnit)
+let builtin_exit =
+  OBuiltIn
+    (function
+    | OInt i -> exit i
+    | _ -> OUnit)
 
 let exit_typ = Scheme ([], TFn (t_int, Infer.new_hole ()))
 
 let arith op =
-  OBuiltIn (function OInt x ->
-    OBuiltIn (function OInt y -> OInt (op x y) | _ -> OUnit) | _ -> OUnit)
+  OBuiltIn
+    (function
+    | OInt x ->
+      OBuiltIn
+        (function
+        | OInt y -> OInt (op x y)
+        | _ -> OUnit)
+    | _ -> OUnit)
 
 let arith_typ = Scheme ([], TFn (t_int, TFn (t_int, t_int)))
-
 let builtin_mul = arith ( * )
 let builtin_plus = arith ( + )
 let builtin_min = arith ( - )
 let builtin_div = arith ( / )
 
-let builtin_inc = OBuiltIn (function OInt i -> OInt (i + 1) | _ -> OUnit)
+let builtin_inc =
+  OBuiltIn
+    (function
+    | OInt i -> OInt (i + 1)
+    | _ -> OUnit)
 
 let inc_typ = Scheme ([], TFn (t_int, t_int))
 
@@ -34,8 +46,7 @@ let type_ctx =
     ("$mul", arith_typ);
     ("inc", inc_typ);
   ]
-  |> List.to_seq
-  |> Infer.Ctx.of_seq
+  |> List.to_seq |> Infer.Ctx.of_seq
 
 let std_env =
   [
@@ -46,23 +57,27 @@ let std_env =
     ("$mul", builtin_mul);
     ("inc", builtin_inc);
   ]
-  |> List.to_seq
-  |> Env.of_seq
+  |> List.to_seq |> Env.of_seq
 
 let rec repl () =
   let () = print_string "> " in
   let s = read_line () in
-  let expr_opt = Parser.program Lexer.read_token (Lexing.from_string s) in
-  let s = match expr_opt with
-    | Some e ->
-      begin try
+  let expr_opt =
+    try Parser.program Lexer.read_token (Lexing.from_string s) with
+    | Lexer.LexError msg ->
+      print_endline msg;
+      None
+  in
+  let s =
+    match expr_opt with
+    | Some e -> (
+      try
         let _ = Infer.infer type_ctx e in
         string_of_object @@ eval std_env e
       with
       | TypeError msg -> msg
-      | EvalException msg -> msg
-      end
-    | None -> "None"
+      | EvalException msg -> msg)
+    | None -> String.empty
   in
   print_endline s;
   repl ()
--- lib/typer/infer.ml
+++ lib/typer/infer.ml
@@ -2,17 +2,14 @@ open Types
 
 let current_level = ref 1
 let current_id = ref 0
-
 let enter_level () = incr current_level
-
 let exit_level () = decr current_level
 
 let new_id () =
   incr current_id;
   !current_id
 
-let new_hole () =
-  THole (ref @@ Unbound (new_id (), !current_level))
+let new_hole () = THole (ref @@ Unbound (new_id (), !current_level))
 
 (** A context to store the types *)
 module Ctx = Map.Make (String)
@@ -28,11 +25,11 @@ module Tbl = Hashtbl.Make (HashableInt)
 let rec replace_typevars tbl = function
   | TVar s -> TVar s
   | TFn (a, b) -> TFn (replace_typevars tbl a, replace_typevars tbl b)
-  | THole ({ contents = Bound t }) -> replace_typevars tbl t
-  | THole ({ contents = Unbound (id, _level) }) as t ->
+  | THole { contents = Bound t } -> replace_typevars tbl t
+  | THole { contents = Unbound (id, _level) } as t -> (
     match Tbl.find_opt tbl id with
     | Some t' -> t'
-    | None -> t
+    | None -> t)
 
 (** Instantiates a type [scheme] into a monotype [typ] *)
 let instantiate (Scheme (binds, t)) =
@@ -43,7 +40,7 @@ let instantiate (Scheme (binds, t)) =
 let rec occurs id level = function
   | TVar _ -> false
   | TFn (a, b) -> occurs id level a || occurs id level b
-  | THole ({ contents = Bound t }) -> occurs id level t
+  | THole { contents = Bound t } -> occurs id level t
   | THole ({ contents = Unbound (id', level') } as t) ->
     let new_level = min level level' in
     t := Unbound (id', new_level);
@@ -52,33 +49,31 @@ let rec occurs id level = function
 let rec type_vars = function
   | TVar _ -> []
   | TFn (a, b) -> type_vars a @ type_vars b
-  | THole ({ contents = Bound t }) -> type_vars t
-  | THole ({ contents = Unbound (id, level) }) ->
-    if level > !current_level then [id] else []
+  | THole { contents = Bound t } -> type_vars t
+  | THole { contents = Unbound (id, level) } ->
+    if level > !current_level then [ id ] else []
 
 let generalize t =
-  type_vars t
-  |> List.sort_uniq compare
-  |> fun binds -> Scheme (binds, t)
+  type_vars t |> List.sort_uniq compare |> fun binds -> Scheme (binds, t)
 
 let rec unify t1 t2 =
   match (t1, t2) with
-  | (TVar a, TVar b) when a = b -> ()
-  | (THole a, THole b) when a = b -> ()
-  | (THole (hole), b) -> unify_hole hole b ~flip:false
-  | (a, THole (hole)) -> unify_hole hole a ~flip:true
-  | (TFn (a, b), TFn (c, d)) -> unify a c; unify b d;
-  | (_, _) -> raise @@ TypeError "Types are not equal"
+  | TVar a, TVar b when a = b -> ()
+  | THole a, THole b when a = b -> ()
+  | THole hole, b -> unify_hole hole b ~flip:false
+  | a, THole hole -> unify_hole hole a ~flip:true
+  | TFn (a, b), TFn (c, d) ->
+    unify a c;
+    unify b d
+  | _, _ -> raise @@ TypeError "Types are not equal"
 
 and unify_hole hole t ~flip =
   match hole.contents with
-  | Bound (t') when flip = true -> unify t t'
-  | Bound (t') -> unify t' t
+  | Bound t' when flip = true -> unify t t'
+  | Bound t' -> unify t' t
   | Unbound (id, level) ->
-    if occurs id level t then
-      raise @@ TypeError "Occurs checking"
-    else
-      hole := Bound t
+    if occurs id level t then raise @@ TypeError "Occurs checking"
+    else hole := Bound t
 
 open Asdf.Ast
 
@@ -103,8 +98,8 @@ let rec infer ctx = function
     enter_level ();
     let t = infer ctx e1 in
     exit_level ();
-    let generalized = (generalize t) in
-    let ctx' = (Ctx.add bind generalized ctx) in
+    let generalized = generalize t in
+    let ctx' = Ctx.add bind generalized ctx in
     let t' = infer ctx' e2 in
     t'
   | EIf (b, e1, e2) ->
